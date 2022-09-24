26f82b02cfebab5a53161fce944b4178e0a887dd
JoseCaste <jotacastellanosguz@gmail.com>
2022-09-24 11:53:00 -0500

    Task 4 & 9

--- src/main/java/com/softserve/sprintone/MainApplication.java
+++ src/main/java/com/softserve/sprintone/MainApplication.java
@@ -1,8 +1,65 @@
 package com.softserve.sprintone;
 
+import com.softserve.sprintone.exception.TaskException;
+import com.softserve.sprintone.jose.TaskJoseSprintOne;
+
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class MainApplication {
+    static final String USER_DIR = "user.dir";
+    static final String taskListPath;
+    static final Scanner scanner = new Scanner(System.in);
+    static final File taskList;
 
-    public static void main (String [] args){
+    static {
+        taskListPath = System.getProperty(USER_DIR).concat("\\").concat("task-list.txt");
+        taskList = new File(taskListPath);
+    }
 
+    public static void main(String[] args) {
+        try {
+            loadFileContent();
+            byte taskNumber = scanner.nextByte();
+            loadTask(taskNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InputMismatchException inputMismatchException) {
+            System.err.println("You has typed a wrong number.");
+        } catch (NoSuchElementException noSuchElementException) {
+            System.err.println("You must type a number from list.");
+        }catch (TaskException taskException){
+            System.err.println(taskException.getMessage());
+        }
+    }
+
+    /**
+     * @param taskNumber task number that has been choosen by user.
+     */
+    private static void loadTask(byte taskNumber) {
+        switch (taskNumber) {
+            case 4:
+                TaskJoseSprintOne.task4(scanner);
+                break;
+            case 9:
+                TaskJoseSprintOne.task9(scanner);
+                break;
+        }
+    }
+
+    /**
+     * Load file content that will be print in console
+     *
+     * @throws IOException
+     */
+    private static void loadFileContent() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(taskList));
+        String st;
+
+        while ((st = br.readLine()) != null)
+
+            System.out.println(st);
     }
 }
--- src/main/java/com/softserve/sprintone/jose/TaskJoseSprintOne.java
+++ src/main/java/com/softserve/sprintone/jose/TaskJoseSprintOne.java
@@ -8,21 +8,21 @@ import java.util.Scanner;
 /**
  * @author JoseCaste
  */
-public class TaskJoseSpringOne {
+public class TaskJoseSprintOne {
 
     /**
      * Task 4 process
      * @param scanner to read values for task
      * @throws TaskException
      */
-    public static void task4(Scanner scanner) throws TaskException {
+    public static void task4(final Scanner scanner) throws TaskException {
         try {
             System.out.println("Type size of the wall in milimiters");
             final int wallSize = scanner.nextInt();
 
             System.out.println("Type size of the pixel in milimiters");
             final int pixelSize = scanner.nextInt();
-            final boolean isDivisible = TaskJoseSpringOne.pixelArtsPlanning(wallSize, pixelSize);
+            final boolean isDivisible = pixelArtsPlanning(wallSize, pixelSize);
             if(isDivisible)
                 System.out.println(" It's fit an exact number of pixels on the wall");
             else
@@ -38,8 +38,30 @@ public class TaskJoseSpringOne {
      * @param pixelSize size of the pixel in millimeters
      * @return true or false if it is possible fit an exact number of pixels on the wall
      */
-    public static boolean pixelArtsPlanning(final int wallSize, final int pixelSize){
-
+    private static boolean pixelArtsPlanning(final int wallSize, final int pixelSize){
         return wallSize % pixelSize == 0;
     }
+
+    /**
+     * Task 4 process
+     * @param scanner to read values for task
+     */
+    public static void task9(final Scanner scanner){
+        try {
+            System.out.println("Type a number that will be negative");
+            int number = scanner.nextInt();
+            makeNegativeNumber(number);
+        } catch (InputMismatchException inputMismatchException){
+            throw new TaskException("Please, choose a correct number");
+        }
+    }
+
+    private static void makeNegativeNumber(final int number) {
+        if(number>=0)
+            System.out.printf("%d", number*(-1));
+        else
+            System.out.printf("%d", number);
+    }
+
+
 }
