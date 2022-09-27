4f86ab62ad2a1f59fc2a7c8f488389760ed16715
cesar-martinezg <poseidon24@hotmail.com>
2022-09-27 09:48:11 -0500

    Test commit

---
+++ src/main/java/com/softserve/javaprogram/cesarm/TaskLuis.java
@@ -0,0 +1,30 @@
+package com.softserve.javaprogram.cesarm;
+
+/**
+ * This is a test class
+ * @author cmart
+ * @lastUpdate 23-Sep-2022
+ */
+public class TaskLuis {
+
+	/**
+	 * This is the sum of two numbers and will return the corresponding value
+	 * @param test1
+	 * @param test2
+	 * @return
+	 * @throws Exception
+	 */
+	public String taskThree(String test1, int test2) throws Exception {
+		
+		
+		// this will return the result of the sum
+		return test1;
+		
+	}
+	
+	
+	public int sumNumbers(int a, int b) {
+		
+	}
+	
+}
